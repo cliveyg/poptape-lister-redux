cb69a515f3795161015481200ac4bccbd1b4c8e6
cliveyg <junkcat@gmail.com>
2025-09-13 14:52:59 -0300

    Add new_api_test.go to aggregate tests and helpers

---
+++ path/to/new_api_test.go
@@ -0,0 +1,34 @@
+// new_api_test.go
+
+package yourpackage
+
+import (
+    // Import necessary packages
+    "testing"
+)
+
+// Helper functions
+func helperFunction1() {
+    // implementation...
+}
+
+// ... other helper functions
+
+// Tests for API
+func TestAPIFunction1(t *testing.T) {
+    // implementation...
+}
+
+// ... other API tests
+
+// Tests for another aspect
+func TestAnotherFunction(t *testing.T) {
+    // implementation...
+}
+
+// Additional tests for missed coverage
+func TestMissedCoverage1(t *testing.T) {
+    // implementation...
+}
+
+// ... other missed coverage tests
